ba4821e8157fb89e2c558f4e620fea708f572ec0
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-16 15:54:35 -0400

    combined reducers with initial state

--- src/index.js
+++ src/index.js
@@ -4,23 +4,32 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// import createStore and combineReducers from redux
 import { combineReducers, createStore } from 'redux';
 
+// products reducer with initial state of []
 function productsReducer(state = [], action) {
   return state;
 }
 
+// user reducer with initial state ''
 function userReducer(state = '', action) {
   return state;
 }
 
+// combining products and user reducers to a single reducer object
 const allReducers = combineReducers({
   products: productsReducer,
   user: userReducer
 });
 
-const store = createStore(allReducers);
+// instantiate a new store with default product iphone and user Yusaku
+const store = createStore(allReducers, {
+  products: [{ name: 'iPhone' }],
+  user: 'Yusaku'
+});
 
+// logs current state of store
 console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
