fe74ba6017f8cc785a9ae737d1d0ee6b03719eab
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-16 15:20:28 -0400

    create an action and dispatching action

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// 1. import createStore from redux
 import { createStore } from 'redux';
 
-function reducer() {
+// 3. new store needs a reducer thats being passed in
+function reducer(state, action) {
   return 'State';
 }
 
+// 4. create an action and dispatching action
+const action = {
+  type: 'changeState',
+  payload: {
+    newState: 'New state'
+  }
+};
+store.dispatch(action);
+
+// 2. set new store to store
 const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
