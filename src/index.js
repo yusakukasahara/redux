dfd4cce2f48c80bcb9f3e908696e118d3a55a249
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-16 18:03:05 -0400

    action file structure organization

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// imported reducers
 import productsReducer from './reducers/products-reducer';
 import userReducer from './reducers/user-reducer';
 
