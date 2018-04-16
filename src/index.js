766f454e40982f9b33d0f21c39dc6ea39b9e2761
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-16 14:15:43 -0400

    import create store from redux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+
+const store = createStore();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
