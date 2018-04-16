a3d07efc7f507a00b9e0d08d11bd23f63add502e
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-16 15:29:59 -0400

    created practical user/products store and added user/products reducers

--- src/index.js
+++ src/index.js
@@ -4,28 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// 1. import createStore from redux
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-// 3. new store needs a reducer thats being passed in
-function reducer(state, action) {
-  if (action.type === 'changeState') {
-    return action.payload.newState;
-  }
-  return 'State';
+function productsReducer(state = [], action) {
+  return state;
 }
 
-// 2. set new store to store
-const store = createStore(reducer);
+function userReducer(state = '', action) {
+  return state;
+}
 
-// 4. create an action and dispatching action
-const action = {
-  type: 'changeState',
-  payload: {
-    newState: 'New state'
-  }
-};
-store.dispatch(action);
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+
+const store = createStore(allReducers);
 
 console.log(store.getState());
 
